e68563dd85cc7955e8e6ef567ceafbf93bb1f539
Jeremy Cuthbert <jeremyajay23@gmail.com>
2026-06-07 15:41:34 -0700

    Revert "Hamburger Navigation Menu"
    This reverts commit a677a8616a9e8f3655f8ebafb40389c6f516e447.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "ORBIC - Oregon Field Guide",
   description: "Discover rare species in Oregon",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-  },
 };
 
 export default function RootLayout({
