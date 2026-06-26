8904d63316718b8b5d42ceefe7c2e57efa22e29e
JohnWinborne <michwinborne@gmail.com>
2026-06-25 18:11:37 -0700

    made the hero background stretch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,14 @@ import ExploreMap from "../components/ExploreMap";
 export default function Home() {
   return (
     <>
-    <div className="mx-auto w-full max-w-7xl">
       <Hero />
-      <main className="w-full px-6 pt-8">
+      <main className="mx-auto w-full max-w-7xl px-6 pt-8">
         <div className="space-y-4">
           <SpeciesCategories />
           <ExploreByHabitat />
           <ExploreMap />
         </div>
       </main>
-      </div>
     </>
   );
 }
