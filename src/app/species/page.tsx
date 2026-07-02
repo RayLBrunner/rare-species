e8a171ee358696c66f1f992075336f15d03e6739
JohnWinborne <michwinborne@gmail.com>
2026-07-01 20:45:45 -0700

    Build responsive species index filters and views

--- src/app/species/page.tsx
+++ src/app/species/page.tsx
@@ -3,7 +3,7 @@ import IndexClient from "@/components/index/IndexClient";
 export default function SpeciesIndexPage() {
   return (
     <main className="bg-white px-4 py-4 text-black">
-      <div className="mx-auto w-full max-w-7xl">
+      <div className="mx-auto w-full max-w-7xl px-6 pt-8">
         <IndexClient />
       </div>
     </main>
