81ce9bb5c6b4476edb63611b0c34b86d21e7e838
Jeremy Cuthbert <jeremyajay23@gmail.com>
2026-07-17 03:30:40 -0700

    Temporary fix to TypeScript cast in species.ts to unblock Vercel build until Alex's new script changes are merged

--- src/lib/species.ts
+++ src/lib/species.ts
@@ -1,7 +1,7 @@
 import data from "@/data/species.json";
 import type { Species } from "@/types/species";
 
-const allSpecies = data as Species[];
+const allSpecies = data as unknown as Species[];
 
 export function getAllSpecies(): Species[] {
   return allSpecies;
